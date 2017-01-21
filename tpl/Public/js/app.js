9b633bbd7342895a6813decdcc4a3f1f247fb565
xinxingyu <xinxingyu@didichuxing.com>
2017-01-21 13:45:26 +0800

    fe

--- tpl/Public/js/app.js
+++ tpl/Public/js/app.js
@@ -3,7 +3,13 @@ $(function(){
 	var touchstart = mobile ? "touchstart" : "mousedown";
 	var touchend = mobile ? "touchend" : "mouseup";
 	var touchmove = mobile ? "touchmove" : "mousemove";
-
+	window.onerror = function(errorMessage, scriptURI, lineNumber,columnNumber,errorObj) {
+    console.log("错误信息：" , errorMessage);
+    console.log("出错文件：" , scriptURI);
+    console.log("出错行号：" , lineNumber);
+    console.log("出错列号：" , columnNumber);
+    console.log("错误详情：" , errorObj);
+  } 
 	var vue = new Vue({
 		el: '#wrapper',
 		data: {
